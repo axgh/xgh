7f1befa915ec831560a35d1dacdb1d95abff29aa
2580412540@qq.com <xiehui00>
2017-05-06 15:01:58 +0800

    a

---
+++ src/net/aaa/cheer/exercise/ch01/HelloWorld.java
@@ -0,0 +1,10 @@
+package net.aaa.cheer.exercise.ch01;
+
+public class HelloWorld {
+	
+	public static void main(String[] args){
+		System.out.println s1("我要学会");
+		System.out.println s2("Java语言");
+	}
+
+}
