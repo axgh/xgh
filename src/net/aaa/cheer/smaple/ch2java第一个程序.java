8e1a1b6224d136f4a5431e0b16d045659e652ad5
2580412540@qq.com <xiehui00>
2017-05-08 08:35:15 +0800

    1

---
+++ src/net/aaa/cheer/smaple/ch2java第一个程序.java
@@ -0,0 +1,7 @@
+package net.aaa.cheer.smaple;
+
+public class ch2java第一个程序 {
+         public static void main(String[]args){
+        	 System.out.println("Hello java");
+         }
+}
---
+++ src/net/aaa/cheer/smaple/ch3java主类结构.java
@@ -0,0 +1,11 @@
+package net.aaa.cheer.smaple;
+
+public class ch3java主类结构 {
+	static String s1="你好";
+	public static void main (String[]args){
+		String s2="java";
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+
+}
