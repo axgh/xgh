0e9e5cf6ab957a96f2f2eae036173e5a363932cd
2580412540@qq.com <xiehui00>
2017-05-14 08:52:12 +0800

    remove old version

--- src/net/aaa/cheer/exercise/ch01/这是我第一个Java程序.java
+++
@@ -1,9 +0,0 @@
-package net.aaa.cheer.exercise.ch01;
-
-import org.junit.Test;
-
-public class 这是我第一个Java程序 {
-		public static void main(String[] args){
-	System.out.println ("这是我第一个Java程序");
-		}
-}
--- src/net/aaa/cheer/smaple/ch01/Hellojava.java
+++
@@ -1,8 +0,0 @@
-package net.aaa.cheer.smaple.ch01;
-
-public class Hellojava {
-       public static void main(String[]args){
-    	   System.out.println("Hello java");
-       }
-       
-}
